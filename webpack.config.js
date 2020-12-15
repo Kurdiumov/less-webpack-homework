18ff820091efbb5ea8829f58555304fee90efdc2
Kurdiumov <Rostyslav.kurdiumov@gmail.com>
2020-12-16 00:01:19 +0100

    Production optimization configured in webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,32 +1,50 @@
 const path = require("path");
-const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
 
-module.exports = {
-  entry: "./src/index.js",
-  output: {
-    filename: "bundle.js",
-    path: path.resolve(__dirname, "dist"),
-    publicPath: "/less-webpack-homework/"
-  },
-  devServer: {
-    contentBase: path.join(__dirname, "dist"),
-    port: 3000,
-    hot: true,
-    inline: true
-  },
-  plugins: [
-    new CleanWebpackPlugin(),
-    new HtmlWebpackPlugin({
-      template: "./src/index.html"
-    })
-  ],
-  module: {
-    rules: [
-      {
-        test: /\.css$/,
-        use: ["style-loader", "css-loader"]
-      }
-    ]
-  }
+module.exports = (env, options) => {
+  const isProd = options.mode === "production";
+
+  return {
+    entry: "./src/index.js",
+    output: {
+      filename: "[name].js",
+      path: path.resolve(__dirname, "dist"),
+      publicPath: "/less-webpack-homework/"
+    },
+    devServer: {
+      publicPath: "/",
+      contentBase: path.join(__dirname, "dist"),
+      port: 3000,
+      hot: true,
+      inline: true
+    },
+    optimization: {
+      minimize: isProd,
+      minimizer: [new TerserPlugin(), new CssMinimizerPlugin()]
+    },
+    plugins: [
+      new CleanWebpackPlugin(),
+      new MiniCssExtractPlugin(),
+      new HtmlWebpackPlugin({
+        template: "./src/index.html",
+        minify: {
+          collapseWhitespace: isProd
+        },
+        filename: "index.html",
+        publicPath: "./"
+      })
+    ],
+    module: {
+      rules: [
+        {
+          test: /\.css$/,
+          use: [MiniCssExtractPlugin.loader, "css-loader", "postcss-loader"]
+        }
+      ]
+    }
+  };
 };
